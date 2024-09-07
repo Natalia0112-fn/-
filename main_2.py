9f7a3b99692c66ab50c530249582f0f02d2b5b73
Natalia0112-fn <keen76@yandex.ru>
2024-09-07 19:53:05 +0300

    Add files via upload

---
+++ main_2.py
@@ -0,0 +1,5 @@
+count_a =12
+count_b =1.5
+name_c = "Python"
+count = count_b/count_a
+print("Курс:",name_c,",","всего задач:",count_a,",","затрачено часов:",count_b,",","средне время выполнения",count,"часа",".")
