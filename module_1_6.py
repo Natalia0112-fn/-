27b8b280a4d2d1c4bc3dd358456f09c4517b4176
Natalia0112-fn <keen76@yandex.ru>
2024-09-15 22:31:26 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,17 @@
+my_dict = {"Natalia": 2000, "Nikolai": 1999, "Petr": 1970}
+print(my_dict)
+print(my_dict['Nikolai'])
+my_dict['Kati']= 2004
+print(my_dict)
+my_dict.update({'Marusia': 2005, 'Konstantin': 1970})
+print(my_dict)
+del my_dict['Natalia']
+print(my_dict)
+
+my_set= {1,2,3,4,1,2,3, 'String', True, (1,2,3,4,5)}
+print(my_set)
+print(my_set.add(9))
+print(my_set)
+print(my_set.discard(1))
+print(my_set)
+
