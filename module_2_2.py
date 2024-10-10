f32dce16937117e4c8abefe65f976a88b9bfb68f
Natalia0112-fn <keen76@yandex.ru>
2024-10-10 14:03:02 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,27 @@
+first = 123
+print(first)
+second = 456
+print(second)
+fhird = 789
+print(fhird)
+if first == second and first == fhird and second == fhird == 3:
+    print("3")
+elif first == second or first == fhird or second == fhird == 2:
+    print("2")
+else:
+    print("0")
+first = 42
+print(first)
+second = 69
+print(second)
+fhird = 42
+print(fhird)
+if first == second and first == fhird and second == fhird == 3:
+    print("3")
+elif first == second or first == fhird or second == fhird == 2:
+    print("2")
+else:
+    print("0")
+
+
+
